83e27b23c35650d4e2c39838116a965f4b391871
Jihye Lee <zerom980706@gmail.com>
2024-06-14 23:49:55 +0900

    프로젝트 생성

---
+++ projectDongbi/src/main/java/com/dongbi/projectDongbi/ProjectDongbiApplication.java
@@ -0,0 +1,13 @@
+package com.dongbi.projectDongbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDongbiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDongbiApplication.class, args);
+	}
+
+}
---
+++ projectDongbi/src/test/java/com/dongbi/projectDongbi/ProjectDongbiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongbi.projectDongbi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDongbiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
