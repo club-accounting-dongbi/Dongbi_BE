3ff4d08a0f5eaae97dc9e310b3bba47ed79b8b66
hyundoo <hyundoo1006@gmail.com>
2024-08-02 17:08:21 +0900

    feat: 비밀번호 변경 기능 구현

--- projectDongbi/src/main/java/com/dongbi/projectDongbi/domain/user/User.java
+++ projectDongbi/src/main/java/com/dongbi/projectDongbi/domain/user/User.java
@@ -22,4 +22,8 @@ public class User {
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "club_id", referencedColumnName = "id")
     private Club club;
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
