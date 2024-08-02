4d329ec78039d57c71ee0d41b0f90fa5165088a8
hyundoo <hyundoo1006@gmail.com>
2024-08-02 17:08:21 +0900

    feat: 비밀번호 변경 기능 구현

--- projectDongbi/src/main/java/com/dongbi/projectDongbi/domain/user/service/UserService.java
+++ projectDongbi/src/main/java/com/dongbi/projectDongbi/domain/user/service/UserService.java
@@ -6,6 +6,7 @@ import com.dongbi.projectDongbi.domain.user.User;
 import com.dongbi.projectDongbi.domain.user.repository.UserRepository;
 import com.dongbi.projectDongbi.web.dto.user.LoginRequestDto;
 import com.dongbi.projectDongbi.web.dto.user.SignupRequestDto;
+import com.dongbi.projectDongbi.web.dto.user.UpdatePasswordRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -21,6 +22,12 @@ public class UserService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final AuthenticationManager authenticationManager;
+
+    public User findById(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException(userId + "에 해당하는 유저가 없습니다."));
+        return user;
+    }
 
     public User signupUser(SignupRequestDto signupRequestDto) {
         if (userRepository.existsByEmail(signupRequestDto.getEmail())) {
@@ -51,4 +58,15 @@ public class UserService {
         PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
         return principalDetails.getUser();
     }
+
+    public void updatePassword(Long userId, UpdatePasswordRequestDto updatePasswordRequestDto){
+        User user = findById(userId);
+        if(!bCryptPasswordEncoder.matches(updatePasswordRequestDto.getOldPassword(), user.getPassword())){
+            throw new IllegalArgumentException("현재 비밀번호가 일치하지 않습니다.");
+        }
+        String encPassword = bCryptPasswordEncoder.encode(updatePasswordRequestDto.getNewPassword());
+        user.setPassword(encPassword);
+        userRepository.save(user);
+    }
+
 }
