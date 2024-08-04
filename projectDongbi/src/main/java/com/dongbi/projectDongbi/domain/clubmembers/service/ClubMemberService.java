4c90c7283413fbe980fe840917c223a31e72a385
changheeBong <model_so@naver.com>
2024-08-04 22:26:35 +0900

    임시

--- projectDongbi/src/main/java/com/dongbi/projectDongbi/domain/clubmembers/service/ClubMemberService.java
+++ projectDongbi/src/main/java/com/dongbi/projectDongbi/domain/clubmembers/service/ClubMemberService.java
@@ -11,8 +11,10 @@ import com.dongbi.projectDongbi.web.clubmembers.dto.request.UpdateClubMemberRequ
 import com.dongbi.projectDongbi.web.clubmembers.dto.response.ClubMemberResponse;
 import com.sun.jdi.request.DuplicateRequestException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 import java.util.NoSuchElementException;
 
