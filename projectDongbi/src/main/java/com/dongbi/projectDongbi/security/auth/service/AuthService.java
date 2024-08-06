2ea060b30744fce1da1451ccf906900ad304a86a
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2024-08-06 15:31:46 +0900

    feat: Login은 JwtAuthenticationFilter에서 관리하게 변경

--- projectDongbi/src/main/java/com/dongbi/projectDongbi/security/auth/service/AuthService.java
+++ projectDongbi/src/main/java/com/dongbi/projectDongbi/security/auth/service/AuthService.java
@@ -3,14 +3,8 @@ package com.dongbi.projectDongbi.security.auth.service;
 import com.dongbi.projectDongbi.domain.club.Club;
 import com.dongbi.projectDongbi.domain.user.User;
 import com.dongbi.projectDongbi.domain.user.repository.UserRepository;
-import com.dongbi.projectDongbi.security.config.auth.PrincipalDetails;
-import com.dongbi.projectDongbi.web.dto.user.LoginRequestDto;
 import com.dongbi.projectDongbi.web.dto.user.SignupRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,18 +13,8 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class AuthService {
 
-    private final AuthenticationManager authenticationManager;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final UserRepository userRepository;
-
-    public User loginUser(LoginRequestDto loginRequestDto){
-        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(loginRequestDto.getEmail(), loginRequestDto.getPassword());
-        Authentication authentication = authenticationManager.authenticate(authToken);
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
-        return principalDetails.getUser();
-    }
 
     @Transactional
     public User signupUser(SignupRequestDto signupRequestDto) {
