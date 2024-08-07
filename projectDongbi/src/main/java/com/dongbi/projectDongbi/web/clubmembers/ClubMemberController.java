273b58188c9a0357ef3ce2278866fe44c1b2d7c8
ChangHee Bong <66407424+changheeBong@users.noreply.github.com>
2024-08-07 13:34:31 +0900

    feat: swagger 추가, 응답객체 추
    * feat: swagger 추가
    
    * feat: 응답객체 추가
    
    기존 Response DTO 를 반환 -> ApiResponse로 감싸서 msg, data로 반환 ClubMemberController / ColController / TransactionController 응답 수정

--- projectDongbi/src/main/java/com/dongbi/projectDongbi/web/clubmembers/ClubMemberController.java
+++ projectDongbi/src/main/java/com/dongbi/projectDongbi/web/clubmembers/ClubMemberController.java
@@ -1,6 +1,7 @@
 package com.dongbi.projectDongbi.web.clubmembers;
 
 import com.dongbi.projectDongbi.domain.clubmembers.service.ClubMemberService;
+import com.dongbi.projectDongbi.global.common.response.ApiResponse;
 import com.dongbi.projectDongbi.web.clubmembers.dto.request.SearchClubMemberRequest;
 import com.dongbi.projectDongbi.web.clubmembers.dto.request.UpdateClubMemberRequest;
 import com.dongbi.projectDongbi.web.clubmembers.dto.response.ClubMemberResponse;
@@ -19,25 +20,25 @@ public class ClubMemberController {
     private final ClubMemberService clubMemberService;
 
     @PostMapping("/new")
-    public ResponseEntity<Void> createClubMember(@RequestBody CreateClubMemberRequest request){
+    public ResponseEntity<ApiResponse<Void>> createClubMember(@RequestBody CreateClubMemberRequest request){
         clubMemberService.createClubMember(request);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(ApiResponse.success());
     }
 
 
 
     @GetMapping("/active")
-    public  ResponseEntity<List<ClubMemberResponse>> findActiveClubMembers(@RequestBody SearchClubMemberRequest request){
+    public  ResponseEntity<ApiResponse<List<ClubMemberResponse>>> findActiveClubMembers(@RequestBody SearchClubMemberRequest request){
 
         List<ClubMemberResponse> result = clubMemberService.findGenerationClubMembers(request);
-        return ResponseEntity.ok(result);
+        return ResponseEntity.ok(ApiResponse.success(result));
     }
 
 
     @PutMapping("update")
-    public ResponseEntity<Void> updateClubMembers(@RequestBody List<UpdateClubMemberRequest> members){
+    public ResponseEntity<ApiResponse<Void>> updateClubMembers(@RequestBody List<UpdateClubMemberRequest> members){
         clubMemberService.updateClubMember(members);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(ApiResponse.success());
     }
 
 }
