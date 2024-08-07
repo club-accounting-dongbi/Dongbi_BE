fcbf435640d8b2043606768556ec45352833cc8a
hyundoo <hyundoo1006@gmail.com>
2024-08-07 20:40:09 +0900

    feat: 반환 dto에 generationNum 추가

--- projectDongbi/src/main/java/com/dongbi/projectDongbi/web/dto/generation/GenerationRequestDto.java
+++ projectDongbi/src/main/java/com/dongbi/projectDongbi/web/dto/generation/GenerationRequestDto.java
@@ -14,6 +14,7 @@ import java.util.List;
 @AllArgsConstructor
 public class GenerationRequestDto {
     private String name;
+    private Long generationNum;
     private Long clubId;
     private LocalDate startDate;
     private LocalDate endDate;
