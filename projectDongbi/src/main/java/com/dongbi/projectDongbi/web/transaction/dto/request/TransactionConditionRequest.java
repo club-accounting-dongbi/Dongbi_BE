cb3fed627a6e0f9032fcf63da7da2ee470b943e4
ChangHee Bong <66407424+changheeBong@users.noreply.github.com>
2024-09-02 21:05:01 +0900

    feat: Transaction swagger api doc 작성 (#14)
    * feat: Transaction swagger api doc 작성

--- projectDongbi/src/main/java/com/dongbi/projectDongbi/web/transaction/dto/request/TransactionConditionRequest.java
+++ projectDongbi/src/main/java/com/dongbi/projectDongbi/web/transaction/dto/request/TransactionConditionRequest.java
@@ -1,6 +1,7 @@
 package com.dongbi.projectDongbi.web.transaction.dto.request;
 
 import com.dongbi.projectDongbi.domain.transaction.Banking;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,13 +13,19 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @Data
 public class TransactionConditionRequest {
+    @Schema(description = "동아리 Id", nullable = false, example = "1")
     private Long clubId;
+    @Schema(description = "동아리 기수 번호", nullable = false, example = "1")
     private Long generationNum;
+    @Schema(description = "시작일자", nullable = true, example = "2024-09-02")
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate startDate;
+    @Schema(description = "종료일자", nullable = true, example = "2024-09-02")
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate endDate;
+    @Schema(description = "이유", nullable = true, example = "할맥")
     private String reason;
+    @Schema(description = "입금 or 출금 ", nullable = true, example = "DEPOSIT , WITHDRAW")
     private Banking banking;
 
 }
