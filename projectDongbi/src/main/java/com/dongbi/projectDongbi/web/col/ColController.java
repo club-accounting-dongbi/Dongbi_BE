fc2ce73933b1c1233ec3b8dad05e1a2ef91bf11c
ChangHee Bong <66407424+changheeBong@users.noreply.github.com>
2024-09-02 21:09:40 +0900

    feat: col api doc 작성 (#15)
    feat: col api doc 작성

--- projectDongbi/src/main/java/com/dongbi/projectDongbi/web/col/ColController.java
+++ projectDongbi/src/main/java/com/dongbi/projectDongbi/web/col/ColController.java
@@ -3,6 +3,8 @@ package com.dongbi.projectDongbi.web.col;
 import com.dongbi.projectDongbi.domain.col.service.ColService;
 import com.dongbi.projectDongbi.global.common.response.ApiResponse;
 import com.dongbi.projectDongbi.web.col.dto.request.ColGenerateRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "컬럼 api 컨트롤러", description = "컬럼 api 컨트롤러" )
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/col")
@@ -19,6 +22,7 @@ public class ColController {
 
     private final ColService colService;
 
+    @Operation(summary = "컬럼 추가", description = "컬럼을 추가합니다.")
     @PostMapping
     public ResponseEntity<ApiResponse<Void>> generateCol(@RequestBody List<ColGenerateRequest> requests){
         colService.generateCol(requests);
