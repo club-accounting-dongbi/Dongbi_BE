0eb6d46b037695a1977e44afcf5a63e78042946e
ChangHee Bong <66407424+changheeBong@users.noreply.github.com>
2024-09-02 21:13:55 +0900

    feat: api doc 추가 (#53)
    feat: Swagger docs 추가
    
    refactor: clubMember length 변경
    
    feat: Transaction swagger api doc 작성
    
    feat: col api doc 작성

--- projectDongbi/src/main/java/com/dongbi/projectDongbi/web/transaction/TransactionController.java
+++ projectDongbi/src/main/java/com/dongbi/projectDongbi/web/transaction/TransactionController.java
@@ -10,6 +10,8 @@ import com.dongbi.projectDongbi.web.transaction.dto.request.DepositRequest;
 import com.dongbi.projectDongbi.web.transaction.dto.request.TransactionConditionRequest;
 import com.dongbi.projectDongbi.web.transaction.dto.request.WithDrawalRequest;
 import com.dongbi.projectDongbi.web.transaction.dto.response.TransactionBankingResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -24,6 +26,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
+@Tag(name = "입출금 컨트롤러", description = "입출금 Api 입니다.")
 @RequiredArgsConstructor
 @RequestMapping("/transactions")
 public class TransactionController {
@@ -32,6 +35,7 @@ public class TransactionController {
     private final FileService fileService;
     private final S3Service s3Service;
 
+    @Operation(summary = "입출금 내역 조회", description = "입출금 내역을 검색조건에 따라 조회합니다.")
     @GetMapping("/search")
     public ResponseEntity<ApiResponse<Page<TransactionBankingResponse>>> getTransactionList(
               TransactionConditionRequest request,
@@ -43,6 +47,7 @@ public class TransactionController {
         return ResponseEntity.ok(ApiResponse.success(result));
     }
 
+    @Operation(summary = "입금", description = "입금")
     @PostMapping("/deposit")
     public ResponseEntity<ApiResponse<Void>> createDeposit(@RequestBody DepositRequest request){
         transactionService.createDeposit(request);
@@ -50,6 +55,7 @@ public class TransactionController {
         return ResponseEntity.ok(ApiResponse.success());
     }
 
+    @Operation(summary = "출금", description = "출금")
     @PostMapping("/withdraw")
     public ResponseEntity<ApiResponse<Void>> createWithdraw(@RequestPart("file") MultipartFile file , @RequestPart WithDrawalRequest request) throws IOException {
         UploadImageResponse savedFile = s3Service.uploadImage(file);
@@ -57,7 +63,7 @@ public class TransactionController {
         return ResponseEntity.ok(ApiResponse.success());
     }
 
-
+    @Operation(summary = "입출금 내역 다운로드", description = "입출금 내역을 pdf나 excel로 다운로드합니다.")
     @GetMapping("download/{type}")
     public ResponseEntity<byte[]> downloadTransaction(@PathVariable String type, @ModelAttribute TransactionConditionRequest request,
                                                       @PageableDefault(size = 10, sort = "occurence_date",direction = Sort.Direction.ASC) Pageable pageable) throws IOException{
