7e32301ff5262432e33072e1b6822959aab1196e
ChangHee Bong <66407424+changheeBong@users.noreply.github.com>
2024-09-05 11:17:49 +0900

    refactor: 엑셀,pdf다운시 전체다운 및 apiDoc 수정 (#16)
    * refactor: 엑셀,pdf다운시 전체다운 및 apiDoc 수정

--- projectDongbi/src/main/java/com/dongbi/projectDongbi/web/transaction/TransactionController.java
+++ projectDongbi/src/main/java/com/dongbi/projectDongbi/web/transaction/TransactionController.java
@@ -11,8 +11,11 @@ import com.dongbi.projectDongbi.web.transaction.dto.request.TransactionCondition
 import com.dongbi.projectDongbi.web.transaction.dto.request.WithDrawalRequest;
 import com.dongbi.projectDongbi.web.transaction.dto.response.TransactionBankingResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -24,6 +27,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @Tag(name = "입출금 컨트롤러", description = "입출금 Api 입니다.")
@@ -38,7 +42,8 @@ public class TransactionController {
     @Operation(summary = "입출금 내역 조회", description = "입출금 내역을 검색조건에 따라 조회합니다.")
     @GetMapping("/search")
     public ResponseEntity<ApiResponse<Page<TransactionBankingResponse>>> getTransactionList(
-              TransactionConditionRequest request,
+            @ParameterObject TransactionConditionRequest request,
+            @Parameter(description = "페이지 정보", schema = @Schema(implementation = Pageable.class))
             @PageableDefault(size = 10, sort = "occurence_date",direction = Sort.Direction.ASC) Pageable pageable
 
     ){
@@ -57,7 +62,10 @@ public class TransactionController {
 
     @Operation(summary = "출금", description = "출금")
     @PostMapping("/withdraw")
-    public ResponseEntity<ApiResponse<Void>> createWithdraw(@RequestPart("file") MultipartFile file , @RequestPart WithDrawalRequest request) throws IOException {
+    public ResponseEntity<ApiResponse<Void>> createWithdraw(
+            @Schema(description = "파일", nullable = false, example = "파일")
+            @RequestPart("file") MultipartFile file
+            , @RequestPart WithDrawalRequest request) throws IOException {
         UploadImageResponse savedFile = s3Service.uploadImage(file);
         transactionService.createWithdraw(request, savedFile.getImagePath());
         return ResponseEntity.ok(ApiResponse.success());
@@ -65,11 +73,16 @@ public class TransactionController {
 
     @Operation(summary = "입출금 내역 다운로드", description = "입출금 내역을 pdf나 excel로 다운로드합니다.")
     @GetMapping("download/{type}")
-    public ResponseEntity<byte[]> downloadTransaction(@PathVariable String type, @ModelAttribute TransactionConditionRequest request,
-                                                      @PageableDefault(size = 10, sort = "occurence_date",direction = Sort.Direction.ASC) Pageable pageable) throws IOException{
-    Page<TransactionBankingResponse> result = transactionService.getTransactionList(request, pageable);
+    public ResponseEntity<byte[]> downloadTransaction(
+            @Parameter(description = "다운로드 type",  example = "pdf / excel")
+            @PathVariable String type,
+            @Parameter(description = "동아리 Id", example = "1")
+            Long clubId,
+            @Parameter(description = "기수번호", example = "1")
+            Long generationNum) throws IOException{
+    List<TransactionBankingResponse> result = transactionService.getAllTransactionList(clubId, generationNum);
 
-        if(result.getContent().isEmpty()){
+        if(result.isEmpty()){
             throw new TransactionException("입출금내역이 없습니다.");
         }
         if(type.equals("excel")){
